5ed6f97f3568de7cffd783cf64b58f521d3228aa
Helder <heldercostaaa@gmail.com>
2020-07-20 22:15:37 -0300

    Create routes file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello StartReact" });
-});
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("✔ Server started on port 3333!");
+  console.log('✔ Server started on port 3333!');
 });
