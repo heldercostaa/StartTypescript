5853705b7350b6de930a79d7b6df82e24203382b
Helder <heldercostaaa@gmail.com>
2020-07-20 22:29:16 -0300

    Configure vscode debugger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello StartReact' });
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
 });
 
 export default routes;
